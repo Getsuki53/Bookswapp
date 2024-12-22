ee7b69acf7e66fb3f7f6e6a9c8730dd3ab45178b
Patricia <patricia.munoz.g@usach.cl>
2024-12-22 18:21:29 -0300

    Update mantenedor_estado.php

--- mantenedor_estado.php
+++ mantenedor_estado.php
@@ -51,6 +51,7 @@ $resultado = $conexion->query("SELECT * FROM estado WHERE Oculto = 1");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mantenedor de Estado</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="mantenedores.css">
 </head>
 <body>
     <div class="sidebar">
