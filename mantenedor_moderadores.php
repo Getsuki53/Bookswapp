c79efa683844fbce1e547d1fdc1329ca569f6f11
Patricia <patricia.munoz.g@usach.cl>
2024-12-22 18:22:01 -0300

    Update mantenedor_moderadores.php

--- mantenedor_moderadores.php
+++ mantenedor_moderadores.php
@@ -51,6 +51,7 @@ $resultado = $conexion->query("SELECT * FROM moderador WHERE Oculto = 1");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mantenedor de Moderadores</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="mantenedores.css">
 </head>
 <body>
     <div class="sidebar">
